966c70dab74b87b8af73da8939ed7d3034de83f1
Oscar <ozserttas.oscar@gmail.com>
2020-09-18 00:35:06 +0200

    add ft_memchr normed

---
+++ ft_memchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/17 17:53:43 by oozsertt          #+#    #+#             */
+/*   Updated: 2020/09/18 00:34:38 by oozsertt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	int		i;
+	char	*res;
+	char	*temp;
+
+	i = 0;
+	res = NULL;
+	temp = (char*)s;
+	while (i < n)
+	{
+		if (*temp == c)
+		{
+			res = temp;
+			return (res);
+		}
+		i++;
+		temp++;
+	}
+	return (res);
+}
