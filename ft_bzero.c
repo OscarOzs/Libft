cf6a3a82c4390e9479c8a9a8940e64528c682ee1
Oscar <ozserttas.oscar@gmail.com>
2020-09-17 15:53:17 +0200

    add ft_bzero normed

---
+++ ft_bzero.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/17 15:23:18 by oozsertt          #+#    #+#             */
+/*   Updated: 2020/09/17 15:52:38 by oozsertt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	int		i;
+	char	*tempforfill;
+
+	i = 0;
+	tempforfill = s;
+	while (i < n)
+	{
+		*tempforfill = '\0';
+		i++;
+		tempforfill++;
+	}
+}
