e4915e5636ef48d53318dfdd9640a9e5d8b26135
Oscar <ozserttas.oscar@gmail.com>
2020-09-17 15:18:37 +0200

    add ft_memset normed

---
+++ ft_memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/17 15:07:18 by oozsertt          #+#    #+#             */
+/*   Updated: 2020/09/17 15:18:01 by oozsertt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	int				i;
+	unsigned char	*tempfill;
+
+	i = 0;
+	tempfill = s;
+	while (i < n)
+	{
+		*tempfill = c;
+		i++;
+		tempfill++;
+	}
+	return (s);
+}
