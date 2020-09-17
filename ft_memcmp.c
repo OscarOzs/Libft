89ef9d839390f03c46128982c782ad782bdc19f0
Oscar <ozserttas.oscar@gmail.com>
2020-09-18 00:59:53 +0200

    add ft_memcmp normed

---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/18 00:37:35 by oozsertt          #+#    #+#             */
+/*   Updated: 2020/09/18 00:58:20 by oozsertt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	char	*ptrs1;
+	char	*ptrs2;
+
+	i = 0;
+	ptrs1 = (char*)s1;
+	ptrs2 = (char*)s2;
+	while (i < n)
+	{
+		if (*ptrs1 != *ptrs2)
+			return (*ptrs1 - *ptrs2);
+		i++;
+		ptrs1++;
+		ptrs2++;
+	}
+	return (0);
+}
