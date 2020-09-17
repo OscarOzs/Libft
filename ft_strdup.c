b4ae265e2e895626f9d1962965710ee232115ea3
Oscar <ozserttas.oscar@gmail.com>
2020-09-18 01:38:00 +0200

    add ft_strdup normed

---
+++ ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/18 01:12:09 by oozsertt          #+#    #+#             */
+/*   Updated: 2020/09/18 01:37:44 by oozsertt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s)
+{
+	int		len;
+	int		i;
+	char	*str;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	str = (char*)malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	return (str);
+}
