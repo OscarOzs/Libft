aa6d9418fbded3a6e279150db05ce12c0508142d
Oscar <ozserttas.oscar@gmail.com>
2020-11-18 16:54:45 +0100

    next -> NULL

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 21:05:49 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/11/18 16:49:27 by oozsertt         ###   ########.fr       */
+/*   Updated: 2020/11/18 16:54:20 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ t_list	*ft_lstnew(void *content)
 	if ((new_node = (t_list*)malloc(sizeof(t_list))) == NULL)
 		return (NULL);
 	new_node->content = content;
+	new_node->next = NULL;
 	return (new_node);
 }
