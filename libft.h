63925c0b5bb48d6efed7f43d0f8eafe712719b83
Oscar <ozserttas.oscar@gmail.com>
2020-11-26 02:09:29 +0100

    ft_lstsize normed and added in libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 19:30:21 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/11/25 15:33:37 by oozsertt         ###   ########.fr       */
+/*   Updated: 2020/11/26 02:08:54 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,7 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **alst, t_list *new);
+int		ft_lstsize(t_list *lst);
 
 
 
