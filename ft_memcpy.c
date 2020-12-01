c7526677b863cb306b40a8c3bfed5eae04c1a62d
Oscar <ozserttas.oscar@gmail.com>
2020-12-01 22:18:35 +0100

    done

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 16:04:22 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/10/13 02:03:50 by oozsertt         ###   ########.fr       */
+/*   Updated: 2020/12/01 22:16:04 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	char	*ptr_dest;
-	char	*ptr_src;
+	unsigned char	*ptr_dest;
+	unsigned char	*ptr_src;
 
 	if (n == 0 || dest == src)
 		return (dest);
 	i = n - 1;
-	ptr_dest = (char*)dest;
-	ptr_src = (char*)src;
+	ptr_dest = (unsigned char*)dest;
+	ptr_src = (unsigned char*)src;
 	while (i > 0)
 	{
 		ptr_dest[i] = ptr_src[i];
 		i--;
 	}
+	ptr_dest[i] = ptr_src[i];
 	return (ptr_dest);
 }
