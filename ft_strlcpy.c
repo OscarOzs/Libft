e7887eca2347c94d77ef540b8b90c259b195a01a
Oscar <ozserttas.oscar@gmail.com>
2020-12-02 15:54:37 +0100

    done

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 17:42:58 by user42            #+#    #+#             */
-/*   Updated: 2020/10/12 19:57:06 by oozsertt         ###   ########.fr       */
+/*   Updated: 2020/12/02 15:48:42 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	int		i;
-	size_t	j;
+	size_t		i;
+	size_t		len_src;
 
 	i = 0;
-	while (dst[i] != '\0')
-		i++;
-	j = 0;
-	while (src[j] != '\0' && j < size)
+	if (dst == NULL || src == NULL)
+		return (0);
+	len_src = ft_strlen(src);
+	if (size > 0)
 	{
-		dst[i] = src[j];
-		i++;
-		j++;
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
 	}
-	dst[i] = '\0';
-	return (i);
+	return (len_src);
 }
