5624d82afef32a552bd3c16e0b0242a576c3c3a3
Oscar <ozserttas.oscar@gmail.com>
2020-12-14 15:36:06 +0100

    put like before

--- ft_memset.c
+++ ft_memset.c
@@ -6,39 +6,24 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 15:07:18 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/12/14 15:34:33 by oozsertt         ###   ########.fr       */
+/*   Updated: 2020/12/14 15:35:56 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	*ft_memset(void *s, int c, size_t n)
-// {
-// 	size_t			i;
-// 	unsigned char	*tempfill;
-
-// 	i = 0;
-// 	tempfill = s;
-// 	while (i < n)
-// 	{
-// 		*tempfill = c;
-// 		i++;
-// 		tempfill++;
-// 	}
-// 	return (s);
-// }
-
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t	i;
-	char	*ptrs;
+	size_t			i;
+	unsigned char	*tempfill;
 
 	i = 0;
-	ptrs = (char*)s;
+	tempfill = s;
 	while (i < n)
 	{
-		ptrs[i] = c;
+		*tempfill = c;
 		i++;
+		tempfill++;
 	}
-	return (ptrs);
+	return (s);
 }
