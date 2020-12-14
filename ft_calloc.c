a1d482b8d089d2af2d754daeb70bf23ee2dd6309
Oscar <ozserttas.oscar@gmail.com>
2020-12-14 15:26:41 +0100

    malloc protected

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 17:45:56 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/10/12 19:57:35 by oozsertt         ###   ########.fr       */
+/*   Updated: 2020/12/14 15:25:59 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char *str;
 
-	str = (char*)malloc(sizeof(size * nmemb));
+	if ((str = (char*)malloc(sizeof(size * nmemb))) == NULL)
+		return (NULL);
 	if (str == NULL)
 		return (NULL);
 	ft_memset(str, 0, (size * nmemb));
