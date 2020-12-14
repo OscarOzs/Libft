6924048c67da9397c376e6f9b21a9884a481a2bc
Oscar <ozserttas.oscar@gmail.com>
2020-12-14 15:55:12 +0100

    easily readable

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 17:45:56 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/12/14 15:47:22 by oozsertt         ###   ########.fr       */
+/*   Updated: 2020/12/14 15:54:46 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char *str;
 
-	if ((str = (char*)malloc(sizeof(char) * (size * nmemb)) == NULL))
+	str = (char*)malloc(sizeof(char) * (size * nmemb));
+	if (str == NULL)
 		return (NULL);
 	str = ft_memset(str, 0, (size * nmemb));
 	return (str);
