2ff810723ad1df176bf2fa9119f9eff7d5c1cf87
Oscar <ozserttas.oscar@gmail.com>
2020-12-29 17:15:09 +0100

    done

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 17:55:47 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/12/17 15:02:10 by oozsertt         ###   ########.fr       */
+/*   Updated: 2020/12/29 17:15:01 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*temp;
+	
 	if (*lst != NULL && del != NULL)
 	{
 		while (*lst != NULL)
 		{
+			temp = (*lst)->next;
 			del((*lst)->content);
-			(*lst) = (*lst)->next;
-			free(*lst);
+			free((*lst));
+			(*lst) = temp;
 		}
 		*lst = NULL;
 	}
