fd2b9483149c3df29292ef5d1f2577e8a4e791fa
Oscar <ozserttas.oscar@gmail.com>
2020-12-30 18:21:04 +0100

    edited

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 03:27:24 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/11/30 17:42:06 by oozsertt         ###   ########.fr       */
+/*   Updated: 2020/12/30 18:20:48 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,35 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 	t_list	*temp;
 
 	if (*alst == NULL)
-		*alst = ft_lstnew(new->content);
+	{
+		*alst = new;
+		new->next = NULL;
+	}
 	else
 	{
 		temp = *alst;
 		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new;
+		new->next = NULL;
+	}
+}
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list *tmp;
+
+	if ((*alst) == NULL)
+	{
+		(*alst) = new;
+		new->next = NULL;
+	}
+	else
+	{
+		tmp = (*alst);
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
 		new->next = NULL;
 	}
 }
