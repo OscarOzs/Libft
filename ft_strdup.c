f3db7e3a620fa7de3f75051a451b01924e4046af
Oscar OZSERTTAS <oozsertt@e3r4p22.42.fr>
2021-01-04 16:44:52 +0100

    prototype edited

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,29 +6,29 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/18 01:12:09 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/10/12 19:57:09 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/01/04 16:44:33 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
+char	*ft_strdup(const char *s1)
 {
 	int		len;
 	int		i;
 	char	*str;
 
 	len = 0;
-	while (s[len] != '\0')
+	while (s1[len] != '\0')
 		len++;
 	str = (char*)malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
 	str[len] = '\0';
 	i = 0;
-	while (s[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		str[i] = s[i];
+		str[i] = s1[i];
 		i++;
 	}
 	return (str);
