b941941e3db1a739bcd1a43cccf2c2c14242f13e
Oscar OZSERTTAS <oozsertt@e3r4p22.42.fr>
2021-01-04 16:32:21 +0100

    last modif

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 17:42:58 by user42            #+#    #+#             */
-/*   Updated: 2020/12/02 15:48:42 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/01/04 16:28:22 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t		i;
 	size_t		len_src;
@@ -21,9 +21,9 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	if (dst == NULL || src == NULL)
 		return (0);
 	len_src = ft_strlen(src);
-	if (size > 0)
+	if (dstsize > 0)
 	{
-		while (src[i] != '\0' && i < (size - 1))
+		while (src[i] != '\0' && i < (dstsize - 1))
 		{
 			dst[i] = src[i];
 			i++;
