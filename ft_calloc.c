1057bb4a40371c6680cc0e4522df1ea1293e1f80
Oscar OZSERTTAS <oozsertt@e3r4p22.42.fr>
2021-01-04 16:42:56 +0100

    prototype edited

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,19 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 17:45:56 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/12/14 15:54:46 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/01/04 16:40:10 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
 	char *str;
 
-	str = (char*)malloc(sizeof(char) * (size * nmemb));
+	str = (char*)malloc(sizeof(char) * (size * count));
 	if (str == NULL)
 		return (NULL);
-	str = ft_memset(str, 0, (size * nmemb));
+	str = ft_memset(str, 0, (size * count));
 	return (str);
 }
