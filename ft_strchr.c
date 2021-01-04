b5112fd33fa58c991cbf953a926426481f5158be
Oscar OZSERTTAS <oozsertt@e3r4p20.42.fr>
2021-01-04 11:04:49 +0100

    work with extended ascii table

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 14:42:27 by user42            #+#    #+#             */
-/*   Updated: 2020/12/01 18:21:37 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/01/04 11:02:17 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ char	*ft_strchr(const char *s, int c)
 	res = NULL;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == (unsigned char)c)
 		{
 			res = (char*)&s[i];
 			return (res);
 		}
 		i++;
 	}
-	if (s[i] == c)
+	if (s[i] == (unsigned char)c)
 	{
 		res = (char*)&s[i];
 		return (res);
