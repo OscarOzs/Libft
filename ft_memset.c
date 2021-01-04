08e28862980a848b4c648e1e6d64aca16cb4c3e3
Oscar OZSERTTAS <oozsertt@e3r4p22.42.fr>
2021-01-04 14:32:03 +0100

    problem with the prototype

--- ft_memset.c
+++ ft_memset.c
@@ -6,24 +6,24 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 15:07:18 by oozsertt          #+#    #+#             */
-/*   Updated: 2021/01/04 11:22:07 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/01/04 14:31:31 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t			i;
 	char			*tempfill;
 
 	i = 0;
-	tempfill = s;
+	tempfill = b;
 	while (i < len)
 	{
 		*tempfill = c;
 		i++;
 		tempfill++;
 	}
-	return (s);
+	return (b);
 }
