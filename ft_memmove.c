c5580c2e1a7b75ad5bf01391d058bbe2b8ec790a
Oscar OZSERTTAS <oozsertt@e3r4p22.42.fr>
2021-01-04 15:52:07 +0100

    prototype edited

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,34 +6,34 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 16:54:35 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/12/29 17:32:13 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/01/04 15:46:24 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char		*ptr_dest;
 	const unsigned char *ptr_src;
 	size_t				i;
 
 	ptr_src = (unsigned char*)src;
-	ptr_dest = (unsigned char*)dest;
+	ptr_dest = (unsigned char*)dst;
 	i = 0;
-	if (src == NULL && dest == NULL)
+	if (src == NULL && dst == NULL)
 		return (NULL);
 	if (ptr_src < ptr_dest)
 	{
 		i++;
-		while (i <= n)
+		while (i <= len)
 		{
-			ptr_dest[n - i] = ptr_src[n - i];
+			ptr_dest[len - i] = ptr_src[len - i];
 			i++;
 		}
 	}
 	else
-		while (i < n)
+		while (i < len)
 		{
 			ptr_dest[i] = ptr_src[i];
 			i++;
