fbf49ff2db8e4fd2b25196c52a1ad4c270d65259
Oscar OZSERTTAS <oozsertt@e3r4p22.42.fr>
2021-01-04 16:25:37 +0100

    last modif

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 17:56:51 by user42            #+#    #+#             */
-/*   Updated: 2020/12/10 18:36:51 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/01/04 16:24:31 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	if (n < 1)
+	if (n == 0)
 		return (0);
 	while (s1[i] == s2[i] && i < (n - 1) && s1[i] != '\0'
 	&& s2[i] != '\0')
