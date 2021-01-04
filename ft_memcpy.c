03c13576764a02944f78de2286757a7dd366aa55
Oscar OZSERTTAS <oozsertt@e3r4p22.42.fr>
2021-01-04 14:38:20 +0100

    prototype edited

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,22 +6,22 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 16:04:22 by oozsertt          #+#    #+#             */
-/*   Updated: 2020/12/10 21:04:49 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/01/04 14:37:47 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *restrict dst, const void *src, size_t n)
 {
 	size_t			i;
 	unsigned char	*ptr_dest;
 	unsigned char	*ptr_src;
 
-	if (n == 0 || dest == src)
-		return (dest);
+	if (n == 0 || dst == src)
+		return (dst);
 	i = n - 1;
-	ptr_dest = (unsigned char*)dest;
+	ptr_dest = (unsigned char*)dst;
 	ptr_src = (unsigned char*)src;
 	while (i > 0)
 	{
